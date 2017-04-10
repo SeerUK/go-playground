fe58644af51a57e75dbd27a2b622a5e78ad065af
Elliot Wright <wright.elliot@gmail.com>
2017-04-10 14:28:43 +0100

    Added some comments.

--- 01_ConCount/main.go
+++ 01_ConCount/main.go
@@ -6,9 +6,7 @@ import (
 	"runtime"
 )
 
-const (
-	iterations = 50000000
-)
+const iterations = 50000000
 
 var cores int
 
@@ -33,17 +31,24 @@ func main() {
 	var ichs []<-chan int // input channels, for initial numbers.
 	var ochs []<-chan int // output channels, for the result chunks.
 
+	// Split the data into as many chunks as we have CPU cores we have access to (note this may not
+	// be all cores on the machine), any more will not be beneficial.
 	for c := 0; c < cpus; c++ {
 		start := c * size
 		end := (c + 1) * size
 
+		// atoc creates channels, but instantly returns them and fills them asynchronously. This
+		// starts the "pipeline".
 		ichs = append(ichs, atoc(is[start:end]))
 	}
 
+	// Run the "pipeline".
 	for _, ich := range ichs {
 		ochs = append(ochs, sum(sq(ich)))
 	}
 
+	// Add all of the separate chunks up, this should be pretty quick because we already added up
+	// the chunks above. We should have to add as many numbers as there are CPU cores.
 	for _, ch := range ochs {
 		for n := range ch {
 			total += n
@@ -67,6 +72,8 @@ func atoc(is []int) <-chan int {
 	return out
 }
 
+// sq asynchronously pulls values from a channel, and pushes that value squared into a new channel
+// that is instantly returned.
 func sq(in <-chan int) <-chan int {
 	out := make(chan int, len(in))
 
@@ -81,6 +88,8 @@ func sq(in <-chan int) <-chan int {
 	return out
 }
 
+// sum asynchronously pulls values from a channel, creates a total, and pushes that into a new
+// channel that is instantly returned.
 func sum(in <-chan int) <-chan int {
 	var total int
 
